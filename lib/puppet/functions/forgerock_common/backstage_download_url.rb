4f52fe8407c13b2589eca1e142c78c751458c284
Hugo Haakseth <hugo.haakseth@spk.no>
2026-08-04 09:14:52 +0200

    chore: prepare to make the module public

--- lib/puppet/functions/forgerock_common/backstage_download_url.rb
+++ lib/puppet/functions/forgerock_common/backstage_download_url.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
+# @summary
+#   This function generates a download URL for a given ForgeRock product based on the provided parameters.
+#
 Puppet::Functions.create_function(:'forgerock_common::backstage_download_url') do
+  # @param product_family_id [String] The product family ID (e.g., 'am').
+  # @param product_id [String] The product ID (e.g., 'openam-web-policy-agents').
+  # @param version [String] The version of the product (e.g., '2025.11', '7.5.0').
+  # @param distribution [String] The distribution type (e.g., 'zip', 'war').
+  # @param release_type [String, nil] The release type (e.g., 'full', 'patch'). Required for some products.
+  # @param platform [String, nil] The platform (e.g., 'apache'). Required for some products.
+  # @param platform_version [String, nil] The platform version (e.g., '2.4'). Required for some products.
+  # @param os [String, nil] The operating system (e.g., 'linux'). Required for some products.
+  # @param architecture [String, nil] The architecture (e.g., '64bit'). Required for some products.
+  # @param include_system_ca_store [Boolean, nil] Whether to include the system CA store when making HTTP requests. Defaults to false.
+  # @return [String] The generated download URL.
   dispatch :backstage_download_url do
     param 'String', :product_family_id
     param 'String', :product_id
@@ -43,15 +57,51 @@ Puppet::Functions.create_function(:'forgerock_common::backstage_download_url') d
                    end
     data = JSON.parse(response.body)['data']
 
-    version_json = version(versions_from_minor_version(minor_version(minor_versions_from_product(product(products_from_family(family(families_from_data(data), product_family_id)), product_id)), dist_version)), version)
+    version_json = version(
+      versions_from_minor_version(
+        minor_version(
+          minor_versions_from_product(
+            product(
+              products_from_family(
+                family(
+                  families_from_data(data), product_family_id
+                ),
+              ), product_id
+            ),
+          ), dist_version
+        ),
+      ), version
+    )
 
-    dist = if product_family_id == 'am' && product_id == 'openam-web-policy-agents'
-             distribution(distributions_from_architecture(architecture(architectures_from_os(os(oses_from_platform_version(platform_version(platform_versions_from_platform(platform(platforms_from_version(version_json), platform)), platform_version)), os)), architecture)), distribution)
-           else
-             distribution(distributions_from_release_type(release_type(release_types_from_version(version_json), release_type)), distribution)
-           end
+    dist_json = if product_family_id == 'am' && product_id == 'openam-web-policy-agents'
+                  distribution(
+                    distributions_from_architecture(
+                      architecture(architectures_from_os(
+                                     os(
+                                       oses_from_platform_version(
+                                         platform_version(
+                                           platform_versions_from_platform(
+                                             platform(
+                                               platforms_from_version(version_json), platform
+                                             ),
+                                           ), platform_version
+                                         ),
+                                       ), os
+                                     ),
+                                   ), architecture),
+                    ), distribution
+                  )
+                else
+                  distribution(
+                    distributions_from_release_type(
+                      release_type(
+                        release_types_from_version(version_json), release_type
+                      ),
+                    ), distribution
+                  )
+                end
 
-    artifact_id = dist['artifactId'] if dist
+    artifact_id = dist_json['artifactId'] if dist_json
     raise Puppet::Error, "Could not find artifact for family_id=#{product_family_id}, product_id=#{product_id}, version=#{version}, platform=#{platform}, platform_version=#{platform_version}, os=#{os}, architecture=#{architecture}, distribution=#{distribution}" if artifact_id.nil?
 
     "#{download_base_url}/#{artifact_id}"
